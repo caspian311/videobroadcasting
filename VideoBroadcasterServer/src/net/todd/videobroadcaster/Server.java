e693b0fa68f9ba557bab92086b805143ddc62e30
Matt Todd <matt.c.todd@gmail.com>
2014-03-09 15:53:27 -0500

    refactored the server a bit

--- VideoBroadcasterServer/src/net/todd/videobroadcaster/Server.java
+++ VideoBroadcasterServer/src/net/todd/videobroadcaster/Server.java
@@ -2,8 +2,8 @@ package net.todd.videobroadcaster;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -21,25 +21,48 @@ public class Server {
 		System.out.println("Listening on port " + port);
 		while (true){
 			System.out.println("Waiting for connection...");
-			Socket socket = serverSocket.accept();
-			System.out.println("Received connection");
-			
-			InputStream in = socket.getInputStream();
-			
-			File outputFile = File.createTempFile("video", ".mp4");
-			System.out.println("Writing to: " + outputFile.getAbsolutePath());
-			FileOutputStream fos = new FileOutputStream(outputFile);
-			
-			byte[] buffer = new byte[1024];
-			int readSize = 0;
-			int totalFileSize = 0;
-			while((readSize = in.read(buffer)) > 0) {
-				fos.write(buffer);
-				totalFileSize += readSize;
-			}
-			fos.close();
-			socket.close();
-			System.out.println("Finished writing " + totalFileSize + " to file");
+			handleConnection(serverSocket.accept());
 		}
+	}
+
+	private void handleConnection(Socket socket) {
+		System.out.println("Received connection");
+		
+		FileOutputStream fos = null;
+		InputStream in = null;
+		try {
+			in = socket.getInputStream();
+			fos = new FileOutputStream(createOutputFile());
+			
+			readInFile(fos, in);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			try {
+				fos.close();
+				socket.close();
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+	private void readInFile(FileOutputStream fos, InputStream in) throws IOException {
+		byte[] buffer = new byte[1024];
+		int totalFileSize = 0;
+		
+		int readSize = 0;
+		while((readSize = in.read(buffer)) > 0) {
+			fos.write(buffer);
+			totalFileSize += readSize;
+		}
+		
+		System.out.println("Finished writing " + totalFileSize + " to file");
+	}
+
+	private File createOutputFile() throws IOException {
+		File outputFile = File.createTempFile("video", ".mp4");
+		System.out.println("Writing to: " + outputFile.getAbsolutePath());
+		return outputFile;
 	}
 }
